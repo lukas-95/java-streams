b5d10a3b41681d690ced7c060214155f88742020
lukas-95 <lucrecmen@gmail.com>
2023-06-25 17:58:15 -0300

    starting mastering streams

--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class GettingStarted {
@@ -17,10 +16,28 @@ public class GettingStarted {
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
         List<Person> people = MockData.getPeople();
+        List<Person> youngers = new ArrayList<>();
+        int total = 10;
+        int count = 0;
+        for (Person person : people) {
+            if (person.getAge() <= 18) {
+                youngers.add(person);
+                count++;
+                if (count == total) {
+                    break;
+                }
+            }
+        }
+        for (Person person : youngers) {
+            System.out.println(person);
+        }
     }
 
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> collected = people.stream().filter(person -> person.getAge() <= 18).limit(10).toList();
+        collected.forEach(System.out::println);
+
     }
 }
--- src/test/java/com/amigoscode/examples/WorkingWithStreams.java
+++ src/test/java/com/amigoscode/examples/WorkingWithStreams.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WorkingWithStreams {
@@ -12,5 +11,18 @@ public class WorkingWithStreams {
     @Test
     void steams() {
         List<String> names = List.of("Amigoscode", "Alex", "Zara");
+
+        Stream<String> stream = names.stream();
+
+        Stream<String> namesStream = Stream.of("Amigoscode", "Alex", "Zara");
+
+        long count = stream
+                .limit(2).map(null).sorted(null).dropWhile(null)
+                .count();
+
+
+        String[] namesArray = { };
+        Arrays.stream(namesArray);
+
     }
 }
